60fecab9be5705acb98bb069b73176ec075fd0c9
San Yung <sankung93@gmail.com>
2020-12-28 00:01:42 -0800

    finished

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
@@ -26,3 +27,10 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
